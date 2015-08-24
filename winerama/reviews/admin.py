08d75e087afcb21bcb0578414f1eab0e75f84130
jadianes <jadianes@gmail.com>
2015-08-24 10:46:55 +0000

    Added custom admin models

--- winerama/reviews/admin.py
+++ winerama/reviews/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Wine, Review
+
+class ReviewAdmin(admin.ModelAdmin):
+    model = Review
+    list_display = ('wine', 'rating', 'user_name', 'comment', 'pub_date')
+    list_filter = ['pub_date', 'user_name']
+    search_fields = ['comment']
+    
+admin.site.register(Wine)
+admin.site.register(Review, ReviewAdmin)
