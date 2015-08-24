cff0a27455ccadf2d1aec903ec1999e7f003f1f6
jadianes <jadianes@gmail.com>
2015-08-24 10:27:41 +0000

    Added model entities

---
+++ winerama/reviews/migrations/0001_initial.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Review',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('pub_date', models.DateTimeField(verbose_name=b'date published')),
+                ('user_name', models.CharField(max_length=100)),
+                ('comment', models.CharField(max_length=200)),
+                ('rating', models.IntegerField(choices=[(1, b'1'), (2, b'2'), (3, b'3'), (4, b'4'), (5, b'5')])),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Wine',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='review',
+            name='wine',
+            field=models.ForeignKey(to='reviews.Wine'),
+        ),
+    ]
--- winerama/winerama/settings.py
+++ winerama/winerama/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'reviews',
 )
 
 MIDDLEWARE_CLASSES = (
